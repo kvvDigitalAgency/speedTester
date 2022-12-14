d20c4d8d49b4fc0130dbef4203b8f0dc9df10211
Скрынников Виктор <vick.skrin.gma@gmail.com>
2022-12-14 20:47:59 +0300

    Добавил класс exception, добавил комментарии, убрал фукнцию вывода инфы, немного рефакторинга

--- speedTester.php
+++ speedTester.php
@@ -1,132 +1,220 @@
 <?php
 
-class speedTester 
+/**
+ *
+ */
+class speedTester
 {
+    /**
+     * @access private
+     * @var array Список добавленных файлов
+     */
     private $files = [];
+    /**
+     * @access private
+     * @var array Список установленных параметров для передачи в функции
+     */
     private $params = [];
+    /**
+     * @access private
+     * @var array Список добавленных функций
+     */
     private $functions = [];
+    /**
+     * @access private
+     * @var int Количество итераций вызова каждой функции
+     */
     private $iterations = 10;
-    private $colLengths = [];
-    private $columns = [
-        'function' => 'Функция',
-        'iterPerSec' => 'Итераций в сек',
-        'time' => 'Время, сек',
-        'coefficient' => 'Коэф',
-        'speedup' => 'Прирост скорости, +%',
-    ];
+    /**
+     * @access private
+     * @var speedTester Созданный единожды объект класса
+     */
     private static $obj;
 
-    public static function create() 
+    /** Функция, реализующая паттерн singleton
+     *
+     * Делает возможным вызов кода из нескольких мест и областей видимостей,
+     * не теряя при этом все подготовленные в другом месте данные
+     *
+     * @access public
+     * @return speedTester
+     */
+    public static function create(): speedTester
     {
         // это задача на внимательность, но мне понравилось
         return self::$obj?:self::$obj = new self();
     }
 
-    public function addFunction(string $functionName) 
+    /**
+     * Добавляются функции, которые нужно протестировать
+     *
+     * @access public
+     * @param array $functionNames Список Функций
+     * @return bool
+     */
+    public function addFunctions(array $functionNames): bool
     {
-        $this->functions = array_merge($this->functions, (array)$functionName);
-    }
-    
-    public function addFile($fileName) 
-    {
-        foreach ($fileName as $name) if(!file_exists($name)) return false;
-        return !!$this->files = array_merge($this->files, (array)$fileName);
-    }
-    
-    public function setParams(array $params) 
-    {
-        $this->params = $params;
-    }
-    
-    public function setCountIterations($count) 
-    {
-        $this->iterations = $count;
+        return !!$this->functions = array_merge($this->functions, $functionNames);
     }
 
-    public function testFunctions($consoleMode = false) 
+    /**
+     * Добавляются файлы, которые нужно подключить
+     *
+     * @access public
+     * @param array $fileNames Список файлов
+     * @return bool
+     * @throws speedTesterException
+     */
+    public function addFiles(array $fileNames): bool
+    {
+        foreach ($fileNames as $name) if(!file_exists($name)) throw new speedTesterException('Нет файла', $name, PHP_EOL);
+        return !!$this->files = array_merge($this->files, $fileNames);
+    }
+
+    /**
+     * Устанавливаются параметры, с которыми функция будет вызываться
+     *
+     * @access public
+     * @param array $params Список параметров
+     * @return bool
+     */
+    public function setParams(array $params): bool
+    {
+        return !!$this->params = $params;
+    }
+
+    /**
+     * Устанавливается количество вызовов каждой функции
+     *
+     * Чем больше значение - тем более точное время выполнения будет рассчитано,
+     * но тестирование будет занимать больше времени.
+     * Малое значение позволит быстро оценить и принять срочное решение.
+     *
+     * @access public
+     * @example "2 функции по 10 итераций занимают ~ 40 сек."
+     * @param int $count Количество итераций
+     * @return bool
+     */
+    public function setCountIterations(int $count): bool
+    {
+        return !!$this->iterations = $count;
+    }
+
+    /**
+     * Расчеты производительности функций
+     *
+     * Производится замер времени выполнения каждой функции, рассчитывается коэффициент,
+     * прирост скорости, результат сортируется. Если включен консольный режим,
+     * то производятся расчеты для вывода информации и ее вывод.
+     *
+     * @access public
+     * @param bool $consoleMode Консольный режим
+     * @return array
+     * @throws speedTesterException
+     */
+    public function testFunctions(bool $consoleMode = false): array
     {
         foreach($this->files as $file) include_once $file;
-        
-        foreach($this->functions as $function) if(!function_exists($function)) {
-            if($consoleMode) echo 'Нет функции', $function, PHP_EOL;
-            return false;
-        }
-        
+
         $params = $this->params;
         $result = [];
         $process = -($percentPerIter = 100 / ($this->iterations * count($this->functions)));
-        
+
         foreach($this->functions as $i => $function) {
+            if(!function_exists($function)) throw new speedTesterException('Нет функции', $function, PHP_EOL);
             $result[$i] = ['function'=>$function,'iterPerSec'=>0];
             for ($j=0; $j < $this->iterations; $j++) {
                 if($consoleMode) echo ' Процесс: ', $process += $percentPerIter, "%\r";
                 for ($t = time(); $t == time(););
-                for (
-                    $result[$i]['iterPerSec'], $t = time(); 
-                    time() == $t; 
-                    $result[$i]['iterPerSec']++
-                ) call_user_func_array($function, $params);
+                for ($t = time(); time() == $t; $result[$i]['iterPerSec']++) call_user_func_array($function, $params);
             }
             $result[$i] += [
                 'time' => 1 / ($result[$i]['iterPerSec'] = round($result[$i]['iterPerSec'] / $this->iterations, 6)),
                 'answer' => call_user_func_array($function, $params)
             ];
         }
-        
+
         usort($result, function($a, $b) {return $b['iterPerSec'] - $a['iterPerSec'];});
-        
+
         $slower = end($result);
-        
+
         foreach($result as &$item) {
             $item['coefficient'] = round($slower['iterPerSec'] / $item['iterPerSec'], 2);
             $item['speedup'] = round($item['iterPerSec'] / $slower['iterPerSec'] * 100 - 100, 2);
         }
-        
-        if(!$consoleMode) return $result;
-        else {
-            array_unshift($result, $this->columns);
+
+        array_unshift($result, $columns = [
+            'function' => 'Функция',
+            'iterPerSec' => 'Итераций в сек',
+            'time' => 'Время, сек',
+            'coefficient' => 'Коэффициент',
+            'speedup' => 'Прирост скорости, +%',
+        ]);
+
+        if($consoleMode) {
             $i = 0;
+            $colLengths = [];
             foreach($result[0] as $k => $v) {
-                foreach(array_merge([$v], array_column($result, $k)) as $V) 
-                    if(mb_strlen($V) > (isset($this->colLengths[$i])?$this->colLengths[$i]:0)) $this->colLengths[$i] = mb_strlen($V);
+                foreach(array_merge([$v], array_column($result, $k)) as $V)
+                    if(mb_strlen($V) > ($colLengths[$i] ?? 0)) $colLengths[$i] = mb_strlen($V);
                 $i++;
             }
-            
-            echo $line = str_repeat('-', array_sum($this->colLengths) + 1 + 3 * count($this->colLengths)) . PHP_EOL;
-           
+
+            echo $line = str_repeat('-', array_sum($colLengths) + 1 + 3 * count($colLengths)) . PHP_EOL;
+
             $line = PHP_EOL . $line;
             $flagSuccess = true;
-            $columns = $this->columns;
-            
+
             foreach($result as $row) {
-                $this->drawString(array_values(array_filter(
-                    array_replace($this->columns, $row), 
-                    function($k) use($columns) {
-                        return isset($columns[$k]);
-                    },
+                $res = [];
+                foreach ($cols = array_values(array_filter(
+                    array_replace($columns, $row),
+                    function($k) use($columns) {return isset($columns[$k]);},
                     ARRAY_FILTER_USE_KEY
-                )));
-                echo $line;
+                )) as $k => $col) $res[] = strlen($col) + $colLengths[$k] - mb_strlen($col);
+                printf('| %-' . implode('s | %-', $res) . 's |' . $line, ...$cols);
                 if(isset($row['answer']) && $row['answer'] != $slower['answer']) $flagSuccess = false;
             }
-            
-            if($flagSuccess) echo 'Ответ одинаковый:' , PHP_EOL , var_export($slower['answer']), PHP_EOL;
+
+            if($flagSuccess) echo 'Ответ одинаковый:' , PHP_EOL , var_export($slower['answer'], true), PHP_EOL;
             else {
                 echo 'Ответ разный:', PHP_EOL;
-                foreach ($result as $row) if(isset($row['answer'])) echo PHP_EOL, 'Функция ', $row['function'], ':', PHP_EOL, var_export($row['answer']), PHP_EOL;
+                foreach ($result as $row) if(isset($row['answer']))
+                    echo PHP_EOL, 'Функция ', $row['function'], ':', PHP_EOL, var_export($row['answer'], true), PHP_EOL;
             }
-            
-            return true;
+
         }
-    }
-    
-    private function drawString($cols) {
-        $res = [];
-        foreach ($cols as $k => $col) $res[] = strlen($col) + $this->colLengths[$k] - mb_strlen($col);
-        printf('| %-' . implode('s | %-', $res) . 's |', ...$cols);
+        array_shift($result);
+        return $result;
     }
 
-    private function __construct() {}
-    private function __sleep() {}
-    private function __wakeup() {}
+    /**
+     * Закрытый конструктор
+     *
+     * @access private
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Закрытая сериализация
+     *
+     * @access private
+     * @return void
+     */
+    private function __sleep()
+    {
+    }
+
+    /**
+     * Закрытая десериализация
+     *
+     * @access private
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
 }
